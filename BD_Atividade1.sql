a1bbe17eb80fe57742f4ae2c43dd13f5334980bf
arielly_g <ariellyg69@gmail.com>
2024-04-06 00:22:30 -0300

    atividade concluída

--- BD_Atividade1.sql
+++ BD_Atividade1.sql
@@ -18,8 +18,6 @@ select Fname, Dno from employee where Dno = (SELECT Dnumber FROM department WHER
 
 insert into department (Dname, Dnumber, Mgr_ssn) values ('DataScience', 7, '987654321');
 
-select * from department;
-
 -- Incluir pelo menos dois cientistas de dados no novo departamento com os atributos básicos:
 
 insert into employee (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) 
@@ -27,8 +25,6 @@ values('Giovana', 'M', 'Teixeira', '998352545', '1969-12-19', '452 Stone, Housto
 
 insert into employee (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) 
 values('Bryan', 'G', 'Oliveira', '998564354', '1979-02-20', '654 Stone, Houston TX', 'M', '37000', '888665555', 7);
-
-select * from employee;
 
 -- Aumentar o salário de todos os empregados em 23.5% do valor atual:
 
@@ -38,6 +34,5 @@ update employee set Salary = Salary * 1.235;
 
 update employee set Address = 'Itapajé, CE' where Dno= 4;
 
-select * from employee;
 
 
